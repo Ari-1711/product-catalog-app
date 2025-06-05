3469e3460b85c29dcf3e1d27cc080fbe93b8e2f8
Ari-1711 <arihermawan006@gmail.com>
2025-06-05 15:31:10 +0700

    Update basename configuration for GitHub Pages routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* <-- Bungkus App dengan BrowserRouter */}
+    <BrowserRouter basename = {process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
